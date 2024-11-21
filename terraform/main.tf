3dc39621c0f63c480c7503b781f2cae430abafcd
ameni <ameni.kallel@edu.techstarter.de>
2024-11-21 22:06:59 +0100

    mycommit1

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,12 @@ resource "aws_security_group" "ssh_access" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress { #eingehender Datenverkehr
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
     egress{
         from_port = 0
